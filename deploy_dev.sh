0f4175926ad9a7300068fc442521e53382eeb167
Janet Rivas-Mendoza (Contractor) <Janet.Rivas.Mendoza@officedepot.com>
2017-06-30 19:02:04 -0400

    +changing path APK

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,6 +1,6 @@
 curl \
 -F "status=2" \
 -F "notify=1" \
--F "ipa=@myapplication/build/outputs/apk/app-debug.apk" \
+-F "ipa=@MyApplication/app/build/outputs/apk/app-debug.apk" \
 -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
 https://rink.hockeyapp.net/api/2/apps/$MyApplicationAppId/app_versions/upload
